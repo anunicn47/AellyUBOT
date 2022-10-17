f5de99f44253d5785a467522dbda13f5d8398788
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-17 12:56:38 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,3 +1,32 @@
+from os import environ
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+HEROKU = bool(
+    environ.get("DYNO")
+)  # NOTE Make it false if you're not deploying on heroku or docker.
+
+if HEROKU:
+
+    BOT_TOKEN = environ.get("BOT_TOKEN", None)
+    API_ID = int(environ.get("API_ID", 6))
+    API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    SESSION_STRING = environ.get("SESSION_STRING", None)
+    USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
+    SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
+    LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
+    GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
+    MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+    WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
+    MONGO_URL = environ.get("MONGO_URL", None)
+    ARQ_API_URL = environ.get("ARQ_API_URL", None)
+    ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", None)))
+    RSS_DELAY = int(environ.get("RSS_DELAY", None))
+    PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
+else:
 BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
 API_ID = 123456
 SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
